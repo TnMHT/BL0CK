610baf5fc50f2ea0facf0aabab4db3f1d9a4d9d0
TeonMehta <teonmehta@gmail.com>
2023-04-16 15:41:21 +0100

    SET UP TYPESCRIPT TO DEPLOY CONTRACT WITH ETHERS. TIMESTAMP: 7:07:36

--- ether-simple-storage/deploy.ts
+++
@@ -1,9 +0,0 @@
-(async () => {
-	console.log("IFFE");
-})();
-
-async function main() {
-	console.log("main");
-    await sleep(1000);
-    console.log("main2");
-}
---
+++ ether-simple-storage/src/deploy.ts
@@ -0,0 +1,30 @@
+import { ethers } from 'ethers';
+import * as fs from 'fs';
+
+(async () => {
+	// COMPILE THEM IN OUR CODE
+	// COMPILE THEM SEPERATELY S
+	// HTTP://127.0.0.1:7545
+})();
+
+async function main() {
+	const rpcServer = 'HTTP://127.0.0.1:7545';
+	const privateKey =
+		'0x6fcac237545f3716d7bd53f984a811df3ede3491ae75385bafb481c35bc3d9b7';
+	// CONNECTION TO THE BLOCKCHAIN
+	const provider = new ethers.JsonRpcProvider(rpcServer);
+	// WALLET WITH OUR PRIVATE KEY TO SIGN DIFFERENT TRANSACTIONS
+	const wallet = new ethers.Wallet(privateKey, provider);
+
+	const abi = fs.readFileSync('./SimpleStorage_sol_SImpleStorage.abi', 'utf8');
+	const binrary = fs.readFileSync(
+		'./SimpleStorage_sol_SImpleStorage.bin',
+		'utf8'
+	);
+	const contractFactory = new ethers.ContractFactory(abi, binrary, wallet);
+	console.log('DEPLOYING CONTRACT. WAIT...');
+	const contract = await contractFactory.deploy(); // STOP HERE. WAIT FOR CONTRACT TO DEPLOY
+	console.log(contract);
+}
+
+main();
