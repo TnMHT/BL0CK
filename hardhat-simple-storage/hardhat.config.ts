6a82c44cb4f1d7b2123256b7f1ad384b36e998cd
TeonMehta <teonmehta@gmail.com>
2023-04-19 23:54:46 +0100

    SUCCESSFULLY DEPLOYED AND VERIFIED CONTRACT TO SEPOLIA TESTNET W/ HARDHAT. TIMESTAMP: 9:11

--- hardhat-simple-storage/hardhat.config.ts
+++ hardhat-simple-storage/hardhat.config.ts
@@ -1,8 +1,26 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomiclabs/hardhat-etherscan';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+	solidity: '0.8.18',
+	networks: {
+		sepolia: {
+			url: SEPOLIA_RPC_URL,
+			accounts: [PRIVATE_KEY],
+		},
+	},
+	etherscan: {
+		// Your API key for Etherscan
+		// Obtain one at https://etherscan.io/
+		apiKey: ETHERSCAN_API_KEY,
+	},
 };
 
 export default config;
--- hardhat-simple-storage/scripts/deploy.ts
+++ hardhat-simple-storage/scripts/deploy.ts
@@ -1,24 +1,54 @@
-import { ethers } from "hardhat";
+import { ethers, network } from 'hardhat';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+	const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+	const SimpleStorageFactory = await ethers.getContractFactory('SimpleStorage');
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+	const SimpleStorage = await SimpleStorageFactory.deploy();
+	await SimpleStorage.deployed();
 
-  await lock.deployed();
+	if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+		// WAIT FOR A FEW BLOCKS
+		console.log('WAITING FOR A FEW BLOCKS');
+		await SimpleStorage.deployTransaction.wait(6);
+		await verify(SimpleStorage.address, []);
+	}
+	const currentValue = await SimpleStorage.retrieve();
+	console.log(`CURRENT VALUE: ${currentValue}`);
+	// UPDATE VALUE
+	const transactionResponse = await SimpleStorage.store(7);
+	await transactionResponse.wait();
+	const updatedValue = await SimpleStorage.retrieve();
+	console.log(`UPDATED VALUE: ${updatedValue}`);
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+	console.log('DEPLOYOING CONTRACT... ');
+	console.log('DEPLOYER ADDRESS:', deployer.address);
+	console.log('ACCOUNT BALANCE:', (await deployer.getBalance()).toString());
+	console.log(`SIMPLE STORAGE ADDRESS: ${SimpleStorage.address}`);
+	console.log(network.config);
+}
+
+async function verify(contractAddress, args) {
+	try {
+		await run('verify:verify', {
+			address: contractAddress,
+			constructorArguments: args,
+		});
+	} catch (e) {
+		if (e.message.toLowerCase().includes('already verified')) {
+			console.log('ALREADY VERIFIED');
+		} else {
+			console.log(e);
+		}
+	}
+
+	console.log('VERIFYING CONTRACT...');
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
