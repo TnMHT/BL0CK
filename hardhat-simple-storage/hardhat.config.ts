aec6d393fa70879655dbe58d5b0778bc4040fb5f
TeonMehta <teonmehta@gmail.com>
2023-04-20 20:15:26 +0100

    GAS REPORTED CONFIG. TIMESTAMP: 9:44:49

--- hardhat-simple-storage/hardhat.config.ts
+++ hardhat-simple-storage/hardhat.config.ts
@@ -3,12 +3,13 @@ import '@nomicfoundation/hardhat-toolbox';
 import '@nomiclabs/hardhat-etherscan';
 import * as dotenv from 'dotenv';
 import { blockNumber } from './tasks/blockNumber';
-
+import 'hardhat-gas-reporter';
 dotenv.config();
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY!;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || '';
+const PRIVATE_KEY = process.env.PRIVATE_KEY! || '';
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || '';
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY || '';
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.18',
@@ -30,6 +31,14 @@ const config: HardhatUserConfig = {
 	},
 	tasks: {
 		blockNumber,
+	},
+	gasReporter: {
+		enabled: true,
+		outputFile: 'gas-reporter.txt',
+		noColors: true,
+		currency: 'GBP',
+		coinmarketcap: COINMARKET_API_KEY,
+		token: 'ETH',
 	},
 };
 
