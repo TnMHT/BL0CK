c7fc0cfac8f651fc97877a35dad443cc7b3b9101
TeonMehta <teonmehta@gmail.com>
2023-04-20 19:11:57 +0100

    ADDED LOCALHOST NETWORK. TIMESTAMP: 9:23:12

--- hardhat-simple-storage/hardhat.config.ts
+++ hardhat-simple-storage/hardhat.config.ts
@@ -12,10 +12,15 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
 	solidity: '0.8.18',
+	defaultNetwork: 'hardhat',
 	networks: {
 		sepolia: {
 			url: SEPOLIA_RPC_URL,
 			accounts: [PRIVATE_KEY],
+		},
+		localhost: {
+			url: 'http://127.0.0.1:8545/',
+			chainId: 31337,
 		},
 	},
 	etherscan: {
