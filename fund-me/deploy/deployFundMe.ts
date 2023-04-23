4be9b59e4175eaa5faeb3a2fb55bfcbee13145a7
TeonMehta <teonmehta@gmail.com>
2023-04-23 09:53:10 +0100

    DEPLOYED FUND ME CONTRACT W/O UTILS. TIMESTAMP:10:52:52

--- fund-me/deploy/deployFundMe.ts
+++ fund-me/deploy/deployFundMe.ts
@@ -1,21 +1,29 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { network } from 'hardhat';
-import { networkConfig } from '../helper.hardhat.config';
+import { developmentChains, networkConfig } from '../helper.hardhat.config';
 
-export const deployMock: DeployFunction = async (
-	hre: HardhatRuntimeEnvironment
-) => {
+const deployFundMe: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 	const { getNamedAccounts, deployments, network } = hre;
-	const { deploy, log } = deployments;
+	const { deploy, log, get } = deployments;
 	const { deployer } = await getNamedAccounts();
 	const chainId = network.config.chainId;
 
-	const ethUsdPriceFeedAddress = networkConfig[chainId]['ethUsdPriceFeed'];
+	let ethUsdPriceFeedAddress;
+	if (developmentChains.includes(network.name)) {
+		const ethUsdAggregator = await get('MockV3Aggregator');
+		ethUsdPriceFeedAddress = ethUsdAggregator.address;
+	} else {
+		ethUsdPriceFeedAddress = networkConfig[chainId]['ethUsdPriceFeed'];
+	}
 
-	const fundMe = await deploy('Fu ndMe', {
+	const fundMe = await deploy('FundMe', {
 		from: deployer,
-		args: [],
+		args: [ethUsdPriceFeedAddress],
 		log: true,
 	});
+
+	log('----------------------------------------------------------------');
 };
+export default deployFundMe;
+module.exports.tags = ['all', 'fundme'];
