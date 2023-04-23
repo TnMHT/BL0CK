0cb9985c64e52e5a90fc1750167b7bdc360d6177
TeonMehta <teonmehta@gmail.com>
2023-04-23 11:15:40 +0100

    SUCCESSFULLY DEPLOYED AND VERIFIED FUND ME CONTRACT ON ETHERSCAN. TIMESTAMP: 11:00:10

--- fund-me/deploy/deployFundMe.ts
+++ fund-me/deploy/deployFundMe.ts
@@ -2,26 +2,35 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { network } from 'hardhat';
 import { developmentChains, networkConfig } from '../helper.hardhat.config';
+import { verify } from '../utils/verify';
 
 const deployFundMe: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 	const { getNamedAccounts, deployments, network } = hre;
 	const { deploy, log, get } = deployments;
 	const { deployer } = await getNamedAccounts();
-	const chainId = network.config.chainId;
+	const chainId: number = network.config.chainId!;
 
 	let ethUsdPriceFeedAddress;
 	if (developmentChains.includes(network.name)) {
 		const ethUsdAggregator = await get('MockV3Aggregator');
 		ethUsdPriceFeedAddress = ethUsdAggregator.address;
 	} else {
-		ethUsdPriceFeedAddress = networkConfig[chainId]['ethUsdPriceFeed'];
+		ethUsdPriceFeedAddress = networkConfig[network.name].ethUsdPriceFeed!
 	}
-
+	const args = [ethUsdPriceFeedAddress];
 	const fundMe = await deploy('FundMe', {
 		from: deployer,
-		args: [ethUsdPriceFeedAddress],
+		args: args,
 		log: true,
+		waitConfirmations:6 , 
 	});
+
+	if (
+		!developmentChains.includes(network.name) &&
+		process.env.ETHERSCAN_API_KEY
+	) {
+		await verify(fundMe.address, args );
+	}
 
 	log('----------------------------------------------------------------');
 };
