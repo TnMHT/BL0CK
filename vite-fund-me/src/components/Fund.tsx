f8264dbec712f629ba6330f9dfd2f77b7f39b0ae
TeonMehta <teonmehta@gmail.com>
2023-05-19 13:35:00 +0100

    ADDED TRANSACTION LISTENER TO WAIT FOR BLOCK MINED. TIMESTAMP: 13:31

--- vite-fund-me/src/components/Fund.tsx
+++ vite-fund-me/src/components/Fund.tsx
@@ -14,10 +14,30 @@ const Fund = () => {
 			const transactionResponse = await contract.fund({
 				value: ethers.parseEther(ethAmount.toString()),
 			});
+			// LISTEN FOR TRANSACTION TO BE MINED
+			// HEY - WAIT FOR TRANSACTION TO FINISH
+			await listenForTransactionMine(transactionResponse, provider);
+			console.log('DONE.');
 		} catch (err) {
 			console.error(err);
 		}
 	};
+	const listenForTransactionMine = (
+		transactionResponse: { hash: any },
+		provider: ethers.BrowserProvider
+	) => {
+		console.log(`MINING ${transactionResponse.hash}....`);
+		// CREATE A LISTENER FOR THE BLOCKCHAIN
+		return new Promise<void>((resolve, reject) => {
+			provider.once(transactionResponse.hash, (transactionReceipt) => {
+				console.log(
+					`COMPLETED WITH ${transactionReceipt.confirmations} CONFIRMATIONS `
+				);
+				resolve();
+			});
+		});
+	};
+
 	return (
 		<>
 			<button onClick={() => fundWallet()}>FUND</button>
